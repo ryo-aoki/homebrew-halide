50661392a49eb8e5d93cbec34303aa75a2fc1d74
Ryo Aoki <r_aoki@fixstars.com>
2017-10-13 02:01:27 +0900

    initial import

---
+++ Formula/halide.rb
@@ -0,0 +1,48 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Halide < Formula
+  desc "a language for image processing and computational photography"
+  homepage "http://halide-lang.org"
+  url "https://github.com/halide/Halide/archive/release_2017_05_03.tar.gz"
+  sha256 "22db6c51ad19d5621ebbada18ab2c6f4da30d6f71c3904aefc302ddadada48f9"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Halide`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+
+  bottle do
+    root_url "https://github.com/ryo-aoki/Halide/releases/download/release_2017_05_03/" # Optional root to calculate bottle URLs
+    sha256 "aad7f6a4f34edf3e7ba08c3a3d9d8b85c89728d9223248ff7afdbc77e7052af2" => :sierra
+  end
+
+  def pour_bottle?
+    # Only needed if this formula has to check if using the pre-built
+    # bottle is fine.
+    true
+  end
+end
